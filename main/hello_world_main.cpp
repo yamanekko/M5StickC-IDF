4229f435ec797b58ddb503c8c62de2b10d408c31
takahashim <maki@rubycolor.org>
2019-10-05 02:53:44 +0900

    add arduino component

--- main/hello_world_main.cpp
+++ main/hello_world_main.cpp
@@ -1,20 +1,12 @@
-/* Hello World Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
+#include "Arduino.h"
 #include <stdio.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
 #include "esp_system.h"
 #include "esp_spi_flash.h"
 
+void setup() {
+    Serial.begin(115200);
 
-void app_main()
-{
+    /* from Hello World Example */
     printf("Hello world!\n");
 
     /* Print chip information */
@@ -29,12 +21,9 @@ void app_main()
 
     printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
             (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
-
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-    printf("Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
+}
+ 
+void loop() {
+    Serial.println("loop");
+    delay(1000);
 }
